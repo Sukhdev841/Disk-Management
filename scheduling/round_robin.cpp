fe297777b492d4ddc49a290c01ebe46ab403a2d0
Sukhdev841 <singhsukhdev1415@gmail.com>
2017-11-14 17:43:57 +0530

    round robin

---
+++ scheduling/round_robin.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void round_robin(int time_quantum,vector<int> processes)
+{
+	int time = 0;
+    int process_count = 0;
+    while(process_count < processes.size())
+    {
+        // first check if 
+	for(int i=0;i<processes.size();i++)
+	{
+		if(arrival[i] <= time && processes[i]>0)
+		{
+			// process is arrived and is not complete
+			if(processess[i] 
+		}
+	}
+    }
+}
+
+
+int main()
+{
+ 
+}
